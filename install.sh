675b001653aee3e6d9892aa9547128d42691c4dd
Arthur Zinck <zinck.arthur4@gmail.com>
2020-12-11 08:31:31 +0100

    feat: install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
+echo "downloading openvpn installer"
 curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh 
+echo "making it executable"
 chmod +x openvpn-install.sh
+echo "launching it headless"
 AUTO_INSTALL=y ./openvpn-install.sh
+echo "copying config in ubuntu home folder"
 cp client.ovpn /home/ubuntu/
+echo "all done, bye !"
 exit
 exit

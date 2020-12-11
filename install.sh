c46b64358f845ed07bec8e4bd24c5d76d5bd26ed
Arthur Zinck <zinck.arthur4@gmail.com>
2020-12-11 08:29:01 +0100

    feat: install script

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-sudo -i
-apt update && apt upgrade -y
 curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh 
 chmod +x openvpn-install.sh
 AUTO_INSTALL=y ./openvpn-install.sh
